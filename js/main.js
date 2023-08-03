6f1d44f73e069c32f878a09a827fe6403a2d5fee
Hossam Ramadan <103776358+HossamRamadan667@users.noreply.github.com>
2023-08-03 15:53:53 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,71 @@
+let leonApp = new Vue({
+	el: "#leon-app",
+	data: {
+		works: [
+			{
+				iconImg: "assets/images/work-icon-1.png",
+				title: "We will do all the work",
+				description:
+					"Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis ex a eligendi odio omnis aperiam, quod doloribus",
+			},
+			{
+				iconImg: "assets/images/work-icon-2.png",
+				title: "Your Product is Worldwide",
+				description:
+					"Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis ex a eligendi odio omnis aperiam, quod doloribus",
+			},
+			{
+				iconImg: "assets/images/work-icon-3.png",
+				title: "Tell Us Your Idea",
+				description:
+					"Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis ex a eligendi odio omnis aperiam, quod doloribus",
+			},
+		],
+		services: [
+			{
+				iconImg: "assets/images/services-icon-1.png",
+				title: "Web Design",
+				description:
+					"Web design encompasses many different skills and disciplines in the production and maintenance of websites",
+			},
+			{
+				iconImg: "assets/images/services-icon-2.png",
+				title: "Graphic Design",
+				description:
+					"Graphic design is the process of visual communication and problem-solving using one or more of typography, photography and illustration.",
+			},
+			{
+				iconImg: "assets/images/services-icon-3.png",
+				title: "UI & UX",
+				description:
+					"Process of enhancing user satisfaction with a product by improving the usability, accessibility, and pleasure provided in the interaction.",
+			},
+			{
+				iconImg: "assets/images/services-icon-4.png",
+				title: "Web Development",
+				description:
+					"Web development is a broad term for the work involved in developing a web site for the Internet or an intranet.",
+			},
+		],
+		projects: [
+			{
+				img: "assets/images/project-1.jpg",
+				title: "Project 1",
+				description:
+					"My creative ability is very difficult to measure because it can manifest in so many surprising and.",
+			},
+			{
+				img: "assets/images/project-2.jpg",
+				title: "Project 2",
+				description:
+					"My creative ability is very difficult to measure because it can manifest in so many surprising and.",
+			},
+			{
+				img: "assets/images/project-3.jpg",
+				title: "Project 3",
+				description:
+					"My creative ability is very difficult to measure because it can manifest in so many surprising and.",
+			},
+		],
+	},
+});
